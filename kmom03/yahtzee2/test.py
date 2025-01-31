1d0257dbc3e0662cc6aca86fe58a1bb10a402512
Filippa Lissäng Öst <filippaliss@gmail.com>
2025-01-31 18:20:12 +0100

    tog bort kmom03

--- kmom03/yahtzee2/test.py
+++
@@ -1,9 +0,0 @@
-"""
-Module to define unit tests for the Yahtzee game.
-"""
-
-import unittest
-
-if __name__ == '__main__':
-    testsuite = unittest.TestLoader().discover("tests")
-    unittest.TextTestRunner(verbosity=2).run(testsuite)
