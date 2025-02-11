69195bf955309396a0c94eee61ad155f77ea448e
zoe waters <zowa22@student.bth.se>
2025-02-11 14:08:37 +0100

    labb validate

--- kmom03/lab2/classes.py
+++ kmom03/lab2/classes.py
@@ -1,4 +1,10 @@
+"""
+hej hopp här har vi klasserna Person, Address,Teacher och Student
+"""
 class Person:
+    """
+    hej hopp här kör person klassen där vi har namn, ssn och adress(om det har)
+    """
     def __init__(self, name, ssn, address=""):
         self.name = name
         self.__ssn = ssn
@@ -6,9 +12,15 @@ class Person:
 
     @property
     def ssn(self):
+        """
+            retunerar ssn
+        """
         return self.__ssn
 
     def set_address(self, address):
+        """
+            ger en adress
+        """
         self.address = address
 
     def __str__(self):
@@ -17,6 +29,9 @@ class Person:
         return f"Name: {self.name} SSN: {self.ssn}"
 
 class Address:
+    """
+    hej hopp här kör Address klassen där vi har city, stare och country
+    """
     def __init__(self, city, state, country):
         self.city = city
         self.state = state
@@ -26,11 +41,17 @@ class Address:
         return f"Address: {self.city} {self.state} {self.country}"
 
 class Teacher(Person):
+    """
+    hej hopp här kör Teacher som har ärvt av klassen person 
+    """
     def __init__(self, name, ssn, address=""):
         super().__init__(name, ssn, address)
         self.courses = []
 
     def add_course(self, course):
+        """
+            lägger till kurs till lärare
+        """
         self.courses.append(course)
 
     def __str__(self):
@@ -44,17 +65,26 @@ class Teacher(Person):
         return f"{base_str} Courses: {courses_str}" if courses_str else base_str
 
 class Student(Person):
+    """
+    hej hopp här kör Student som har ärvt av klassen person 
+    """
     def __init__(self, name, ssn, address=""):
         super().__init__(name, ssn, address)
         self.courses_grades = []
 
     def add_course_grade(self, course, grade):
+        """
+            lägger till ett betyg till kurs
+        """
         self.courses_grades.append((course, grade))
 
     def average_grade(self):
+        """
+            räknar ut och retunerar medelbetyget för kurserna.
+        """
         total = 0
         count = 0
-        for course, grade in self.courses_grades:
+        for _, grade in self.courses_grades:
             if grade != "-":
                 total += grade
                 count += 1
