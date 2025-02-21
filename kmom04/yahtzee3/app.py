8893d095ccb3289243d21286298013c776d89238
Filippa Lissäng Öst <filippaliss@gmail.com>
2025-02-21 19:45:51 +0100

    fix

--- kmom04/yahtzee3/app.py
+++ kmom04/yahtzee3/app.py
@@ -2,12 +2,13 @@
 Module to define the Hand class.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, session
 from src.scoreboard import Scoreboard
 from src.hand import Hand
 
-app = Flask(__name__)
+app = Flask(name)
 
+app.secret_key = 'zoeofilippaarsjalvmordsbenagnaochelinmed'
 
 @app.route("/")
 def main():
@@ -17,9 +18,21 @@ def main():
     Returns:
         str: Rendered HTML template.
     """
-    hand = Hand()
+    if "scoreboard" not in session:
+        # Skapa en ny Scoreboard om det inte finns i sessionen
+        scoreboard = Scoreboard()
+        session["scoreboard"] = scoreboard.to_json()  # Spara som JSON-sträng
+    else:
+        # Ladda Scoreboard från sessionen
+        scoreboard_data = session["scoreboard"]
+        scoreboard = Scoreboard.from_json(scoreboard_data)
+
+    if "hand" not in session:
+        session["hand"] = Hand().to_list()
+
+    hand = Hand(dice_values=session["hand"])
     scoreboard = Scoreboard()
-    return render_template('index.html', hand=hand)
+    return render_template('index.html', hand=hand, scoreboard=scoreboard)
 
 @app.route("/about")
 def about():
@@ -34,6 +47,7 @@ def about():
 @app.route("/reset")
 def reset():
     hand = Hand()
+    session.clear()
     return render_template('index.html', hand=hand)
 
 @app.route("/choose_rule", methods=["POST"])
@@ -46,5 +60,5 @@ def choose_rule():
             flash(str(e), "error")
     return redirect(url_for("index"))
 
-if __name__ == '__main__':
+if name == 'main':
     app.run(debug=True)
