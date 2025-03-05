247f714b2f33919ad690323133bdda239e8483ce
Filippa Lissäng Öst <filippaliss@gmail.com>
2025-03-05 14:21:52 +0100

    fixade kmom05

--- kmom05/yahtzee4/src/unorderedlist.py
+++ kmom05/yahtzee4/src/unorderedlist.py
@@ -4,67 +4,112 @@
 """
 from src.errors import MissingIndex, MissingValue
 
+class Node:
+    """
+    A class representing a node in a linked list.
+    """
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
 class UnorderedList:
     """
-    A class representing the UnorderedList.
+    A class representing the UnorderedList using a linked list.
     """
     def __init__(self):
-        self.items = []
+        self.head = None
+        self._size = 0
 
     def append(self, value):
         """
-        Adds a value to the list.
+        Adds a value to the end of the list.
         """
-        self.items.append(value)
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = new_node
+        self._size += 1
 
     def add(self, value):
         """
-        Lägger till ett värde i listan. (Same functionality as append)
+        Alias for append.
         """
         self.append(value)
 
     def get(self, index):
         """
-        Hämtar värdet på den angivna indexpositionen i listan.
+        Gets the value at the specified index.
         """
-        if index < 0 or index >= len(self.items):
+        if index < 0 or index >= self._size:
             raise MissingIndex("Index out of bounds.")
-        return self.items[index]
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        return current.value
 
     def remove(self, value):
         """
-        Tar bort ett värde från listan om det finns.
+        Removes a value from the list if present.
         """
-        if value not in self.items:
-            raise MissingValue("Value not found.")
-        self.items.remove(value)
+        current = self.head
+        previous = None
+
+        while current:
+            if current.value == value:
+                if previous:
+                    previous.next = current.next
+                else:
+                    self.head = current.next
+                self._size -= 1
+                return
+            previous = current
+            current = current.next
+
+        raise MissingValue("Value not found.")
 
     def size(self):
         """
-        Return the number of items in the list.
+        Returns the number of items in the list.
         """
-        return len(self.items)
+        return self._size
 
     def index_of(self, value):
         """
         Returns the index of the given value in the list.
         Raises MissingValue if the value is not found.
         """
-        if value not in self.items:
-            raise MissingValue("Value not found.")
-        return self.items.index(value)
+        current = self.head
+        index = 0
+        while current:
+            if current.value == value:
+                return index
+            current = current.next
+            index += 1
+        raise MissingValue("Value not found.")
 
     def print_list(self):
         """
-        Prints all the elements in the list.
+        Prints all elements in the list.
         """
-        print(self.items)
+        elements = []
+        current = self.head
+        while current:
+            elements.append(current.value)
+            current = current.next
+        print(elements)
 
     def set(self, index, value):
         """
         Sets the value at the given index in the list.
         Raises MissingIndex if the index is out of bounds.
         """
-        if index < 0 or index >= len(self.items):
+        if index < 0 or index >= self._size:
             raise MissingIndex("Index out of bounds.")
-        self.items[index] = value
+        current = self.head
+        for _ in range(index):
+            current = current.next
+        current.value = value
