18397e92a667e3ed1a76e290e61490d237ee5163
zoe waters <zowa22@student.bth.se>
2025-03-05 14:50:50 +0100

    leader

--- kmom06/yahtzee5/src/leaderboard.py
+++ kmom06/yahtzee5/src/leaderboard.py
@@ -2,7 +2,6 @@
 leaderboard filen, Hålla reda på spelarnas namn och poäng i en leaderboard.
 """
 from src.unorderedlist import UnorderedList
-from src.sort import recursive_insertion
 
 class Leaderboard:
     """
@@ -13,47 +12,33 @@ class Leaderboard:
 
     def add_player(self, name, score):
         """
-            Lägger till en spelare i en tuple
+        Lägger till en spelare i en tuple.
         """
         self.players.add((name, score))
 
     def remove_player(self, name):
         """
-            Tar bort en spelare från listan baserat på namn
+        Tar bort en spelare från listan baserat på namn.
         """
-        for player in self.players.items:
-            if player[0] == name:
-                self.players.remove(player)
-                break
-
-    def get_top_players(self):
-        """
-        Sortera spelarna baserat på poäng i fallande ordning.
-        """
-        self.players.head = self.recursive_insertion(self.players.head)
-        return self.players
-
-    #def sort_players_by_score(self, players):
-    #    """
-    #    Sortera spelarna baserat på poäng i fallande ordning.
-    #    """
-    #    players_list = []
-    #    current = players.head
-    #    while current:
-    #        players_list.append(current.data)
-    #        current = current.next
-    #    return sorted(players_list, key=self.get_score, reverse=True)
+        current = self.players.head
+        while current:
+            if current.value[0] == name:
+                self.players.remove(current.value)
+                return
+            current = current.next
 
     def get_score(self, player):
         """
-            Hämtar poängen för en spelare (som är den andra värdet i tuplen)
+        Hämtar poängen för en spelare (som är den andra värdet i tuplen).
         """
         return player[1]
 
     def save_to_file(self):
         """
-            Spara leaderboarden till en fil
+        Spara leaderboarden till en fil.
         """
         with open("leaderboard.txt", "w", encoding="utf-8") as f:
-            for player in self.players.items:
-                f.write(f"{player[0]}: {player[1]}\n")
+            current = self.players.head
+            while current:
+                f.write(f"{current.value[0]}: {current.value[1]}\n")
+                current = current.next
